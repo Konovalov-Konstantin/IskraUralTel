47281211c15523a1ad3c92a89d60d21101adedc1
Konovalov Konstantin <kon-kos87@yandex.ru>
2023-08-12 12:48:38 +0500

    init

---
+++ mks/src/main/java/com/mks/mks/MksApplication.java
@@ -0,0 +1,13 @@
+package com.mks.mks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MksApplication.class, args);
+    }
+
+}
